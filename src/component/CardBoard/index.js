acf9f4571bc00f7220a65a569d1a1f612382a926
SyedUmerHasan <umerhasan2010@gmail.com>
2019-06-14 17:56:16 +0500

    Snake Game Completed with results Game Over V-Fd

--- src/component/CardBoard/index.js
+++ src/component/CardBoard/index.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import CardBoardItem from '../CardBoardItem';
 import ArrowKeysReact from 'arrow-keys-react';
 import { Alert } from 'antd';
+import { array } from 'prop-types';
 
 
 
@@ -23,7 +24,7 @@ export default class CardBoard extends Component {
             left: () => {
             console.log("I am left");
                 if (this.state.activeValue_X <= 0) {
-
+                    //  activeValue_X not go less than 0
                 } else {
                     this.setState({
                         activeValue_X: this.state.activeValue_X - 1
@@ -32,8 +33,8 @@ export default class CardBoard extends Component {
             },
             right: () => {
                 console.log("I am right");
-                if (this.state.activeValue_X >= this.props.boardSize) {
-
+                if (this.state.activeValue_X >= this.props.boardSize-1) {
+                    //  activeValue_X not go greater than 20
                 } else {
                     this.setState({
                         activeValue_X: this.state.activeValue_X + 1
@@ -43,7 +44,7 @@ export default class CardBoard extends Component {
             up: () => {
             console.log("I am up");
                 if (this.state.activeValue_Y <= 0) {
-      
+                          //  activeValue_Y not go less than 0
                 } else {
                     this.setState({
                         activeValue_Y: this.state.activeValue_Y - 1
@@ -54,7 +55,7 @@ export default class CardBoard extends Component {
             console.log("I am down");
                 if (this.state.activeValue_Y >= this.props.boardSize-1 )
                 {
-
+                    //  activeValue_Y not go graeter than 20
                 }
                 else{
                     this.setState({
@@ -73,23 +74,80 @@ export default class CardBoard extends Component {
             boardArray: boardArray,
             xAxisEnemy: this.getRandomInt(19),
             yAxisEnemy: this.getRandomInt(19),
-            gameOver : false
+            gameOver : false,
+            Enemies: this.createRandomEnemies,
         };
-
     }
+
+
+
+
+    // Creates Random enemies
+    createRandomEnemies = () => {
+        var Enemies = []
+        for (let index = 0; index < 10; index++) {
+            let singleEnemy = {
+                "xAxisEnemyPosition": this.getRandomInt(19),
+                "yAxisEnemyPosition": this.getRandomInt(19)
+            }
+            Enemies.push(singleEnemy)
+        }
+        console.log('====================================');
+        console.log("I am enemies");
+        console.log('====================================');
+        return Enemies
+    }
+    // Closes the Game Over Message that appear on the Top of the Game 
     onClose = e => {
         console.log(e, 'I was closed.');
     };
 
+    componentDidMount() {
+        var array = this.createRandomEnemies()
+        this.setState({
+            Enemies: array
+        })
+    }
 
+    addingEnemy = (boardArray, enemyPositionArray) => {
+        console.log("I am Adding Enemies");
+        return boardArray
+    }
+    updatePositionPlayer = (boardArray, playerPositionArray, enemyPositionArray) => {
+        if (Array.isArray(enemyPositionArray) == true)
+        {
+            boardArray[playerPositionArray.yAxisPlayerPosition][playerPositionArray.xAxisPlayerPosition] = < CardBoardItem {...ArrowKeysReact.events} styleSheet = "ifColorActive"/>
+            enemyPositionArray.forEach((enemyItem, EnemyNumber) => {
+                boardArray[enemyItem.yAxisEnemyPosition][enemyItem.xAxisEnemyPosition] = < CardBoardItem {...ArrowKeysReact.events}styleSheet = "ifColorActive"/>
+            })
+            this.checkGameOver(playerPositionArray, enemyPositionArray)
+        }
+        else{
+            
+            console.log("This is not an array ")
+        }
+        return boardArray
+    }
+    checkGameOver = (playerPositionArray, enemyPositionArray) => {
+        console.log('====================================');
+        console.log("I am  checking Game Over");
+        console.log('====================================');
+        enemyPositionArray.forEach((enemyItem, EnemyNumber) => {
+            if (enemyItem.xAxisEnemyPosition == this.state.activeValue_X && enemyItem.yAxisEnemyPosition == this.state.activeValue_Y )
+            {
+                console.log("Game Kahatm")
+                this.state.gameOver = true
+            }
+        })
+    }
     getRandomInt = (max) => {
         return Math.floor(Math.random() * Math.floor(max));
     }
+
     render() {
-        {console.log(this.state.boardArray)}
         let gameOverPrintArray = <Alert
-                message="Error Text"
-                description="Error Description Error Description Error Description Error Description Error Description Error Description"
+                message="Game Over"
+                description="You spawn with enemy, he killed you."
                 type="error"
                 closable
                 onClose={this.onClose}
@@ -98,17 +156,36 @@ export default class CardBoard extends Component {
         var yAxisMovement = this.state.activeValue_Y;
         var xAxisEnemyMovement = this.state.xAxisEnemy;
         var yAxisEnemyMovement = this.state.yAxisEnemy;
+
         var boardArray = this.state.boardArray;
-        if (yAxisEnemyMovement == yAxisMovement && xAxisEnemyMovement == xAxisMovement)
-        {
-            if(this.state.gameOver != true)
-            {
-                this.setState({
-                    gameOver : true
-                });
-            }
-            console.log("Game End");
-        }
+
+        let boardElementArray = [];
+        this.state.boardArray.map(function(item, iterator){
+            boardElementArray.push([]);
+        });
+
+        this.state.boardArray.map(function (item, index) {
+            var myArray = [];
+            var length = 0;
+            let myItem = item.map(function (newItem, iterator) {
+                // Add all array compartments with component to print black box
+                boardElementArray[index][iterator] = < CardBoardItem {
+                    ...ArrowKeysReact.events
+                }
+                styleSheet = "ifColorNotActive" / > ;
+                length++;
+            });
+            boardElementArray[index][length] = < br/>
+                return myArray;
+        })
+
+        // Updating Enemy and Player position in dummy Array 
+        boardElementArray = this.updatePositionPlayer(boardElementArray, {
+            xAxisPlayerPosition: this.state.activeValue_X,
+            yAxisPlayerPosition: this.state.activeValue_Y,
+        }, this.state.Enemies)
+
+
         return (
         <div>
 
@@ -116,48 +193,13 @@ export default class CardBoard extends Component {
                 {this.state.gameOver == true ? gameOverPrintArray  : null}
             </div>
             <div {...ArrowKeysReact.events} tabIndex="1">
-
-
-                {this.state.boardArray.map(function (item,index){
-                    var myArray = []; 
-                    let myItem = item.map(function (newItem,iterator) {
-                            // myArray.push(newItem)
-                            if (xAxisMovement == iterator || xAxisEnemyMovement == iterator)
-                            {
-                                if (xAxisMovement == iterator )
-                                {
-                                    if (index == yAxisMovement ) {   
-                                        if (yAxisEnemyMovement == yAxisMovement && xAxisEnemyMovement == xAxisMovement)
-                                        {
-                                            console.log("Game Over");
-                                        }
-                                        console.log('====================================');
-                                        console.log(index , "=" , xAxisMovement);
-                                        console.log(yAxisMovement , "=" , yAxisEnemyMovement);
-                                        // console.log(iterator);
-                                        console.log('====================================');
-                                        // Player
-                                        myArray.push (<CardBoardItem {...ArrowKeysReact.events} styleSheet="ifColorActive"/>)
-                                    }
-                                }
-                                else{
-                                    myArray.push(<CardBoardItem {...ArrowKeysReact.events} styleSheet="ifColorNotActive"/>)
-                                }
-                            }
-                            else{
-                                myArray.push( <CardBoardItem {...ArrowKeysReact.events} styleSheet="ifColorNotActive"/>)
-                            }
-                    });
-                    // console.log(myItem)
-                    myArray.push(<br/>)
-                    return myArray;
-                })}
+            {
+            }
+            {boardElementArray}
             </div>
             <h1>X-axis : {xAxisMovement}</h1>
             
             <h1>Y-axis : {yAxisMovement}</h1>
-
-
 
             <h1>The Enemy at X  :  {this.state.xAxisEnemy}  and  Y :  {this.state.yAxisEnemy}</h1>
         </div>
